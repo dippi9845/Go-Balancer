ab7446b2cbd2dc2e37c114bbd54bad6b3ca870e6
Filippo Di Pietro <filippodp9845@gmail.com>
2023-10-25 00:42:47 +0200

    Update main.go
    defined functions on Backend

--- main.go
+++ main.go
@@ -27,3 +27,18 @@ type Backend struct {
 	mux          sync.RWMutex
 	ReverseProxy *httputil.ReverseProxy
 }
+
+// SetAlive for this backend
+func (b *Backend) SetAlive(alive bool) {
+	b.mux.Lock()
+	b.Alive = alive
+	b.mux.Unlock()
+}
+
+// IsAlive returns true when backend is alive
+func (b *Backend) IsAlive() (alive bool) {
+	b.mux.RLock()
+	alive = b.Alive
+	b.mux.RUnlock()
+	return
+}
