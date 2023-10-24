2e12910287316ef8e772f8af06c20bd018e3fdc5
Filippo Di Pietro <filippodp9845@gmail.com>
2023-10-25 00:47:46 +0200

    Update main.go
    defined MarkBackendStatus GetNextPeer

--- main.go
+++ main.go
@@ -58,3 +58,30 @@ func (s *ServerPool) AddBackend(backend *Backend) {
 func (s *ServerPool) NextIndex() int {
 	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
 }
+
+// MarkBackendStatus changes a status of a backend
+func (s *ServerPool) MarkBackendStatus(backendUrl *url.URL, alive bool) {
+	for _, b := range s.backends {
+		if b.URL.String() == backendUrl.String() {
+			b.SetAlive(alive)
+			break
+		}
+	}
+}
+
+// GetNextPeer returns next active peer to take a connection
+func (s *ServerPool) GetNextPeer() *Backend {
+	// loop entire backends to find out an Alive backend
+	next := s.NextIndex()
+	l := len(s.backends) + next // start from next and move a full cycle
+	for i := next; i < l; i++ {
+		idx := i % len(s.backends) // take an index by modding
+		if s.backends[idx].IsAlive() { // if we have an alive backend, use it and store if its not the original one
+			if i != next {
+				atomic.StoreUint64(&s.current, uint64(idx))
+			}
+			return s.backends[idx]
+		}
+	}
+	return nil
+}
